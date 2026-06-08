6e2bb787c361635efd60f6943c952a7001c28032
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2026-06-08 12:03:46 +0300

    feat: Add Face Exercise related models

--- backend/models.py
+++ backend/models.py
@@ -32,7 +32,47 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class ProTip(models.Model):
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=255)
+    content = fields.TextField()
+
+    class Meta:
+        table = "pro_tips"
+
+    def __str__(self):
+        return self.title
+
+class Benefit(models.Model):
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=255)
+    description = fields.TextField()
+
+    class Meta:
+        table = "benefits"
+
+    def __str__(self):
+        return self.title
+
+class Routine(models.Model):
+    id = fields.IntField(pk=True)
+    icon = fields.CharField(max_length=10)
+    level = fields.CharField(max_length=50)
+    title = fields.CharField(max_length=255)
+    target = fields.CharField(max_length=100)
+    steps = fields.JSONField()
+    timing = fields.CharField(max_length=100)
+
+    class Meta:
+        table = "routines"
+
+    def __str__(self):
+        return self.title
+
 BlogPost_Pydantic = pydantic_model_creator(BlogPost, name="BlogPost")
 BlogPostIn_Pydantic = pydantic_model_creator(BlogPost, name="BlogPostIn", exclude_readonly=True)
 Product_Pydantic = pydantic_model_creator(Product, name="Product")
 ProductIn_Pydantic = pydantic_model_creator(Product, name="ProductIn", exclude_readonly=True)
+Benefit_Pydantic = pydantic_model_creator(Benefit, name="Benefit")
+Routine_Pydantic = pydantic_model_creator(Routine, name="Routine")
+ProTip_Pydantic = pydantic_model_creator(ProTip, name="ProTip")
